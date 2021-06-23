aefbb89413b11ce7e1cca345651ace0abff56734
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-23 19:08:54 +0530

    alioth: overlay: Create power profile for xiaomi SM8250-AC
    Change-Id: I2adfa9dee45351801b9ea33a40d554d98b7db371

--- device.mk
+++ device.mk
@@ -368,6 +368,9 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-tequila
+    
+PRODUCT_PACKAGES += \
+    AliothFrameworks 
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
