b8b377ea749e99dd79db9455caa034a57e2748ff
fiirefly24 <muhammadalifikri2412@gmail.com>
2023-02-04 16:20:07 +0000

    alioth: Time for tequila!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/tequila_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    tequila_alioth-user \
+    tequila_alioth-userdebug \
+    tequila_alioth-eng
