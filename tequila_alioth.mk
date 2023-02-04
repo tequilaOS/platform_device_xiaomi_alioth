e648eb842d99d09c8b9f4165e359552ed095fc04
fiirefly24 <muhammadalifikri2412@gmail.com>
2023-02-04 16:20:07 +0000

    alioth: Time for tequila!

--- tequila_alioth.mk
+++ tequila_alioth.mk
@@ -8,17 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common tequila stuff.
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := tequila_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
+
+TEQUILA_BUILDTYPE := OFFICIAL
+
+# Device Specific
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
